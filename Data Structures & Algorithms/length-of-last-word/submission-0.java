41eedc0d7a3e1c55af25f36ab8aac879474de48d
Harshit <157937871+harshitkachhadia@users.noreply.github.com>
2026-07-02 09:22:12 +0530

    Add: length-of-last-word - submission-0

---
+++ Data Structures & Algorithms/length-of-last-word/submission-0.java
@@ -0,0 +1,10 @@
+class Solution {
+    public int lengthOfLastWord(String s) {
+        String[] word = s.split(" ");
+        String lastWord = word[word.length - 1];
+
+        return lastWord.length();
+
+       
+    }
+}
